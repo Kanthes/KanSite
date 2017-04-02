1af5619ba351430d7839b2ca3031b0e1924f635f
kanthes <permattsson42@gmail.com>
2017-04-02 23:15:37 +0200

    More testing of Selenium browser emulator.

--- records/management/commands/SeleniumTwitch.py
+++ records/management/commands/SeleniumTwitch.py
@@ -2,11 +2,14 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 
-browser = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true', '--ssl-protocol=any'])
+browser = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true', '--ssl-protocol=any'], desired_capabilities=dcap)
 browser.set_page_load_timeout(10)
 print "Browser created."
 browser.get("http://www.twitch.tv/login")
 print "Loaded initial site."
+
+soup = BeautifulSoup(browser.page_source, 'html.parser')
+print [input_element['name'] for input_element in soup.find_all('input')]
 
 element = browser.find_element_by_name("username")
 element.send_keys("kanthes")
@@ -20,7 +23,7 @@ try:
 	element = browser.find_element_by_name("authy_token")
 except:
 	soup = BeautifulSoup(browser.page_source, 'html.parser')
-	print soup.find_all('input')
+	print [input_element['name'] for input_element in soup.find_all('input')]
 	print browser.current_url
 	print soup.prettify()
 element.send_keys(raw_input("Please enter the 6-digit code:"))
