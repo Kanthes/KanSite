7a484c851fe28fbbdec0f58257d62880d684fdc1
kanthes <permattsson42@gmail.com>
2017-04-03 15:18:37 +0200

    Initial celery commit

--- KanCompDetector_site/settings.py
+++ KanCompDetector_site/settings.py
@@ -109,3 +109,9 @@ TEMPLATES = [
         },
     },
 ]
+
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
